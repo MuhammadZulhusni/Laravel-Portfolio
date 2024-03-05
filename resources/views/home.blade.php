7adcba25938c0714af4c69fe9fae95d1c6145fe3
Admin <admin@Admins-MacBook-Air.local>
2024-03-05 15:13:27 +0800

    Learning

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 @section('container') 
     <div class="welcome-text">
         <center>
-            Welcome Testing <br> To <br> My Portfolio
+            Welcome Testing sjaa <br> To <br> My Portfolio
         </center>
     </div>
     
